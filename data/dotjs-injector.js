ea9edd56b4ab6b9e6481769e86631376abae953b
canuckistani <jeff@canuckistani.ca>
2014-04-07 11:09:32 -0700

    4 => 2 spaces pt 3

--- data/dotjs-injector.js
+++ data/dotjs-injector.js
@@ -2,34 +2,34 @@
  * catch the 'load-scripts' event and inject the results into the current scope.
  */
 (function() {
-    self.port.on("load-scripts", function(msg) {
-        // bail out if we're in an iframe
-        if (window.frameElement) return;
-        
-        if (msg.jquery) {
-            eval(msg.jquery);
-        }
-
-        if (msg.js) {
-            eval(msg.js);
-        }
-
-        if (msg.coffee) {
-            (function() {
-                eval(msg.transpiler);
-            }).call(window); // coffee-script.js assumes this === window
-            eval(CoffeeScript.compile(msg.coffee));
-        }
-
-        if (msg.css) {
-            var headNode = document.querySelector('head');
-            var cssNode = document.createElement('style');
-            cssNode.innerHTML = msg.css;
-            headNode.appendChild(cssNode);
-        }
-    });
-
-    if (document.URL.indexOf('http') === 0) {
-        self.port.emit('init', document.URL);    
+  self.port.on("load-scripts", function(msg) {
+    // bail out if we're in an iframe
+    if (window.frameElement) return;
+    
+    if (msg.jquery) {
+      eval(msg.jquery);
     }
+
+    if (msg.js) {
+      eval(msg.js);
+    }
+
+    if (msg.coffee) {
+      (function() {
+        eval(msg.transpiler);
+      }).call(window); // coffee-script.js assumes this === window
+      eval(CoffeeScript.compile(msg.coffee));
+    }
+
+    if (msg.css) {
+      var headNode = document.querySelector('head');
+      var cssNode = document.createElement('style');
+      cssNode.innerHTML = msg.css;
+      headNode.appendChild(cssNode);
+    }
+  });
+
+  if (document.URL.indexOf('http') === 0) {
+    self.port.emit('init', document.URL);    
+  }
 })();
